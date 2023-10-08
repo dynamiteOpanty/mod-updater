7b238cacb76455a5b2063c4e26d5506b11ec4638
dynamiteOpanty <103298123+dynamiteOpanty@users.noreply.github.com>
2023-10-08 14:24:45 +0900

    server progress

--- APIserver.py
+++ APIserver.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI
-from json import load
+import dynaModules
+
 app = FastAPI()
 
 @app.get("/items/{item}")
 def read_item(item: str):
     if item == "mods":
-        serverjson = ReadJsonFile("./datas/server.json")
-        return serverjson["mods"]
-
-# jsonファイルをpythonオブジェクトとして読み込みます 辞書型じゃね多分
-def ReadJsonFile(path: str):
-    with open(path, 'r') as file:
-        return load(file)
+        serverjson = dynaModules.ReadJsonFile("./server/mods.json")
+        return serverjson
--- mod-updater.py
+++ mod-updater.py
@@ -1,9 +1,9 @@
 #version 0.0
 
 import menu
+import urllib
+import json
 import dynaModules
-from urllib import request as urlrequest
-from json import load, loads, dumps
 from os import path as ospath, mkdir
 import asyncio
 
@@ -13,11 +13,19 @@ def main():
     instancePath = config["instancePath"]
     if not ospath.exists("./datas"):
         mkdir("./datas")
-    dynaModules.WriteJsonFile("./datas/test1.json", ModrinthAPIRequest("project/P7dR8mSH"))
-    dynaModules.WriteJsonFile("./datas/test2.json", ModrinthAPIRequest("project/P7dR8mSH/version"))
+    dynaModules.WriteJsonFile("./datas/test3.json", MainServerAPIRequest("mods"))
+    # dynaModules.WriteJsonFile("./datas/test1.json", ModrinthAPIRequest("project/Xbc0uyRg"))
+    # dynaModules.WriteJsonFile("./datas/test2.json", ModrinthAPIRequest("project/Xbc0uyRg/version"))
 
 def ModrinthAPIRequest(target: str):
     return dynaModules.reqget(f'https://api.modrinth.com/v2/{target}', header= {"User-Agent" : "mod updater py \n self study by a student contact: matunyan0930@gmail.com"})
+
+def MainServerAPIRequest(target: str):
+    try:
+        return dynaModules.reqget(f"http://127.0.0.1:8000/items/{target}")
+    except urllib.error.URLError:
+        print("couldn't reach the server. make sure the IP is correct, or expect server is down now.")
+        return {"detail": "error, couldn't reach the server"}
 
 if __name__=="__main__":
     main()
