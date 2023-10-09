7adfa1df43776e1b5653f86050e2cb52e7fc0907
dynamiteOpanty <103298123+dynamiteOpanty@users.noreply.github.com>
2023-10-09 12:19:03 +0900

    error solve

--- dynaModules.py
+++ dynaModules.py
@@ -1,21 +1,27 @@
-from urllib import request as urlrequest
+from urllib import request as urlrequest, error as urlliberror
 from json import load, loads, dumps
 from os import path as ospath, mkdir
 
+class reqgeterror(Exception):
+    pass
 # APIリクエストの本体です
 def reqget(URL: str, debug: bool = False, header = None):
     if not header == None:
         get_req = urlrequest.Request(URL, headers=header)
     else:
         get_req = urlrequest.Request(URL)
-    with urlrequest.urlopen(get_req) as res:
-        body = loads(res.read())
-        if debug:
-            print(body)
-        return body
+    try:
+        with urlrequest.urlopen(get_req) as res:
+            body = loads(res.read())
+            if debug:
+                print(body)
+            return body
+    except urlliberror.URLError:
+        print("hey")
+        raise reqgeterror
 
 # jsonファイルを生成し書き込みます 返り値は書き込んだ内容　よくテストしてないんだけど多分引数は辞書型でも辞書型みたいな面した文字型でもok
-def WriteJsonFile(filepath: str, content: list):
+def WriteJsonFile(filepath: str, content: list | dict):
     if not ospath.splitext(filepath)[1] == ".json":
         print("path doesn't end with '.json', please fix it later.")
         filepath = filepath + ".json"
--- mod-updater.py
+++ mod-updater.py
@@ -5,6 +5,7 @@ import urllib
 import json
 import dynaModules
 from os import path as ospath, mkdir
+import shutil
 import asyncio
 
 def main():
@@ -16,6 +17,10 @@ def main():
     dynaModules.WriteJsonFile("./datas/test3.json", MainServerAPIRequest("mods"))
     # dynaModules.WriteJsonFile("./datas/test1.json", ModrinthAPIRequest("project/Xbc0uyRg"))
     # dynaModules.WriteJsonFile("./datas/test2.json", ModrinthAPIRequest("project/Xbc0uyRg/version"))
+    mods = dynaModules.ReadJsonFile("./datas/test3.json")
+    content = [f'{i["title"]}' for i in mods["mods"]]
+    menu.scroll(screen_lines=min(len(content) + 2, shutil.get_terminal_size().lines), content= content)
+
 
 def ModrinthAPIRequest(target: str):
     return dynaModules.reqget(f'https://api.modrinth.com/v2/{target}', header= {"User-Agent" : "mod updater py \n self study by a student contact: matunyan0930@gmail.com"})
@@ -23,7 +28,7 @@ def ModrinthAPIRequest(target: str):
 def MainServerAPIRequest(target: str):
     try:
         return dynaModules.reqget(f"http://127.0.0.1:8000/items/{target}")
-    except urllib.error.URLError:
+    except dynaModules.reqgeterror:
         print("couldn't reach the server. make sure the IP is correct, or expect server is down now.")
         return {"detail": "error, couldn't reach the server"}
 
